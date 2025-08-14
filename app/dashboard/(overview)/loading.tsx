9337ae36bfa7d42f8f219fceae51e08cbb5ab6f0
Ahmad Hamze <ahmadhamze@yahoo.com>
2025-08-14 21:36:24 +0300

    Complete chapter 9

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+// * This file exists in (overview) folder (not the parenthesis), so that it doesn't apply to the pages in customers/ and invoices/
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
