07ca36f01be3f8aaf0c35c477a2f0d21cd2ca087
Ahmad Hamze <ahmadhamze@yahoo.com>
2025-08-25 20:51:30 +0300

    Complete chapter 12

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,28 @@
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+  let customers: any = [];
+  try {
+    customers = await fetchCustomers();
+  } catch (error) {
+    console.error("Error fetching customers:", error);
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
